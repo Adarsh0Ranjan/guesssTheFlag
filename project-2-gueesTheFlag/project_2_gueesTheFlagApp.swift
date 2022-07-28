a6ace4ff7e908bf51225794e0e65ef6d787c57c4
adarsh <adarsh0ranjan@gmail.com>
2022-07-28 17:12:46 +0530

    Initial Commit

---
+++ project-2-gueesTheFlag/project_2_gueesTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_2_gueesTheFlagApp.swift
+//  project-2-gueesTheFlag
+//
+//  Created by Roro Solutions on 28/07/22.
+//
+
+import SwiftUI
+
+@main
+struct project_2_gueesTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
